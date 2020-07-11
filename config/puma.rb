8d23b58d8e4b0859938354cfdd19a8d691f7fa7b
vagrant <vagrant@localhost.localdomain>
2020-07-11 00:42:08 +0000

    [fix]puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -38,15 +38,15 @@ plugin :tmp_restart
 
 bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 rails_root = Dir.pwd
-#本番環境のみデーモン起動
+# 本番環境のみデーモン起動
 if Rails.env.production?
-    pidfile File.join(rails_root, 'tmp', 'paid', 'puma.pid')
-    state_path File.join(rails_root, 'tmp', 'paid', 'puma.state')
-    stdout_redirect(
-        File.join(rails_root, 'log', 'puma.log'),
-        File.join(rails_root, 'log', 'puma-error.log')
-        true
-    )
-    #デーモン
-    daemonize
+  pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
+  state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+  stdout_redirect(
+    File.join(rails_root, 'log', 'puma.log'),
+    File.join(rails_root, 'log', 'puma-error.log'),
+    true
+  )
+  # デーモン
+  daemonize
 end
